9139b7f7f599f50431a69e95e071d086743651ec
xinqiu <xinqiu.94@gmail.com>
2016-01-18 19:17:49 +0800

    完成数据储存和读取

--- Checklists/ ChecklistItem.swift
+++ Checklists/ ChecklistItem.swift
@@ -8,11 +8,26 @@
 
 import Foundation
 
-class ChecklistItem: NSObject {
+class ChecklistItem: NSObject, NSCoding {
     var text = ""
     var checked = false
     
     func toggleChecked(){
         checked = !checked
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        text = aDecoder.decodeObjectForKey("Text") as! String
+        checked = aDecoder.decodeBoolForKey("Checked")
+        super.init()
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(text, forKey: "Text")
+        aCoder.encodeBool(checked, forKey: "Checked")
     }
 }
